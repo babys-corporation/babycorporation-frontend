7bd2e32fa529e5904ad99fc6c0373ac730051da6
pedropg22 <fafnir.pgv@gmail.com>
2026-04-06 15:40:49 -0300

    header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,49 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+
+  <body>
+    <header>
+      <img src="/public/baby.png" alt="baby">
+      <h1>BabyCorporation</h1>
+    </header>
+    <section class="encontreababa">
+      <h2>Encontre a babá perfeita para sua família</h2>
+      <p>Conectamos você com babás qualificadas, verificadas e de confiança.
+        Cuidado profissional para seus pequenos.</p>
+      <img src="/menina-balanco.png" alt="menina-balanco">
+      <button>
+        Encontrar Babás
+      </button>
+      <button>
+        Cadastrar como Babá
+      </button>
+    </section>
+  </body>
+
 </template>
 
-<style scoped></style>
+<style scoped>
+/*====================
+         body
+=====================*/     
+body{
+  align-items: center;
+  text-align: center;
+} 
+/*====================
+        header        
+=====================*/
+header{
+  display: flex;
+  align-items: center;
+  color: FFFFFF;
+}
+header h1{
+  font-size: 1.2rem;
+}
+header img{
+width: 10%;
+height: 10%;
+}
+</style>
