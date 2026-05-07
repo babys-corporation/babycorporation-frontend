cad63879fb49016cba9b434c743ace51fde62259
danielhenrique090109 <danielhenrique090109@gmail.com>
2026-05-07 15:42:39 -0300

    criação dos componentes de BabaCard r FiltroBabas

---
+++ src/componentes/cards/BabaCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import BabaCard from './BabaCard.vue'
+
+const babas = [
+  {
+    nome: 'Patrícia Alves',
+    cidade: 'Brasília, DF',
+    descricao: 'Mais de 10 anos de experiência...',
+    experiencia: '10 anos de experiência',
+    foto: '/patricia.png',
+    tags: ['Psicologia Infantil'],
+    verificada: true
+  },
+  // ... outras babás
+]
+</script>
+
+<template>
+  <div class="lista">
+    <BabaCard
+      v-for="baba in babas"
+      :key="baba.nome"
+      v-bind="baba"
+    />
+  </div>
+</template>
