1ca8c1c85bcf9f7e9c90f1fe5e69ccce9104a378
danielhenrique090109 <danielhenrique090109@gmail.com>
2026-08-11 15:08:48 -0300

    adiciona .env ao gitignore

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  // Tira o texto com aspas e coloca o import do Vite
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
